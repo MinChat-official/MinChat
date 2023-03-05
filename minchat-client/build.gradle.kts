69c6d863f852f7409b94ca62c79bb11a21fe5700
Mnemotechnician <daytimer253@gmail.com>
2023-03-05 03:05:53 +0300

    Incremental dexing! Woohoo!

--- minchat-client/build.gradle.kts
+++ minchat-client/build.gradle.kts
@@ -1,3 +1,7 @@
+import java.io.OutputStream
+import java.security.MessageDigest
+import java.util.Base64
+
 plugins {
 	kotlin("jvm") version "1.8.0"
 }
@@ -20,6 +24,12 @@ dependencies {
 
 /** Android-specific stuff. Do not modify unless you're 100% sure you know what you're doing! If you break this task, mobile users won't be able to use your mod!*/
 task("jarAndroid") {
+	fun hash(data: ByteArray): String =
+		MessageDigest.getInstance("MD5")
+		.digest(data)
+		.let { Base64.getEncoder().encodeToString(it) }
+		.replace('/', '_')
+
 	dependsOn("jar")
 	
 	doLast {
@@ -53,18 +63,93 @@ task("jarAndroid") {
 			println("using ${platformRoot.absolutePath}")
 		}
 		
-		
 		//collect dependencies needed to translate java 8 bytecode code to android-compatible bytecode (yeah, android's dvm and art do be sucking)
 		val dependencies = 
-			(configurations.compileClasspath.get().files() + configurations.runtimeClasspath.get().files() + File(platformRoot, "android.jar"))
+			(configurations.runtimeClasspath.get().files)
 			.map { it.path }
+		
+		val dexRoot = File("$buildDir/dex/").also { it.mkdirs() }
+		val dexCacheRoot = dexRoot.resolve("cache").also { it.mkdirs() }
+
+		// read the dex cache map (path-to-hash)
+		val dexCacheHashes = dexRoot.resolve("listing.txt")
+			.takeIf { it.exists() }
+			?.readText()
+			?.lineSequence()
+			?.map { it.split(" ") }
+			?.filter { it.size == 2 }
+			?.associate { it[0] to it[1] }
+			.orEmpty()
+			.toMutableMap()
+
+		// calculate hashes for all dependencies
+		val hashes = dependencies
+			.associate {
+				it to hash(File(it).readBytes())
+			}
+
+		// determime which dependencies can have their cached dex files reused and which can not
+		val reusable = ArrayList<String>()
+		val needReDex = HashMap<String, String>() // path-to-hash
+		hashes.forEach { (path, hash) ->
+			if (dexCacheHashes.getOrDefault(path, null) == hash) {
+				reusable += path
+			} else {
+				needReDex[path] = hash
+			}
+		}
+
+		println("${reusable.size} dependencies are already desugared and can be reused.")
+		if (needReDex.isNotEmpty()) println("Desugaring ${needReDex.size} dependencies.")
+
+		// for every non-reusable dependency, invoke d8 and save the new hash
+		var index = 1
+		needReDex.forEach { (dependency, hash) ->
+			println("Processing ${index++}/${needReDex.size} ($dependency)")
+
+			val outputDir = dexCacheRoot.resolve(hash(dependency.toByteArray())).also { it.mkdir() }
+			exec {
+				errorOutput = OutputStream.nullOutputStream()
+				commandLine(
+					"d8",
+					"--intermediate",
+					"--classpath", "${platformRoot.absolutePath}/android.jar",
+					"--min-api", "14", 
+					"--output", outputDir.absolutePath, 
+					dependency
+				)
+			}
+			println()
+			dexCacheHashes[dependency] = hash
+		}
+
+		// write the updated hash map to the file
+		dexCacheHashes.asSequence()
+			.map { (k, v) -> "$k $v" }
+			.joinToString("\n")
+			.let { dexRoot.resolve("listing.txt").writeText(it) }
+
+		if (needReDex.isNotEmpty()) println("Done.")
+		println("Preparing to desugar the project and merge dex files.")
+
+		// finally, assemble instructions for the final d8 to merge the previously generated dex files
+		val dexPathes = dependencies.map { "${dexCacheRoot.absolutePath}/${hash(it.toByteArray())}" }
 		val dependenciesStr = Array<String>(dependencies.size * 2) {
-			if (it % 2 == 0) "--classpath" else dependencies.elementAt(it / 2)
+			if (it % 2 == 0) "--classpath" else dexPathes[it / 2]
 		}
 		
+		// and finally, merge everything and desugar the project
 		exec {
 			workingDir("$buildDir/libs")
-			commandLine("d8", *dependenciesStr, "--min-api", "14", "--output", "${jarName}-android.jar", "${jarName}-desktop.jar")
+			errorOutput = OutputStream.nullOutputStream()
+			commandLine(
+				"d8", 
+				*dependenciesStr, 
+				"--classpath", "${platformRoot.absolutePath}/android.jar",
+				"--min-api", "14", 
+				"--output", "${jarName}-android.jar", 
+				"${jarName}.jar"
+			)
 		}
 	}
 }
@@ -77,22 +162,21 @@ task<Jar>("release") {
 	archiveFileName.set("$jarName-any-platform.jar")
 
 	from(
-		zipTree("$buildDir/libs/${jarName}-desktop.jar"),
+		zipTree("$buildDir/libs/${jarName}.jar"),
 		zipTree("$buildDir/libs/${jarName}-android.jar")
 	)
 
+	from(*configurations.runtimeClasspath.get().files.map { if (it.isDirectory()) it else zipTree(it) }.toTypedArray())
+
 	doLast {
-		delete { delete("$buildDir/libs/${jarName}-desktop.jar") }
+		delete { delete("$buildDir/libs/${jarName}.jar") }
 		delete { delete("$buildDir/libs/${jarName}-android.jar") }
 	}
 }
 
-
 tasks.jar {
 	duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-	archiveFileName.set("${jarName}-desktop.jar")
-
-	from(*configurations.runtimeClasspath.get().files.map { if (it.isDirectory()) it else zipTree(it) }.toTypedArray())
+	archiveFileName.set("${jarName}.jar")
 
 	from(rootDir) {
 		include("mod.hjson")
