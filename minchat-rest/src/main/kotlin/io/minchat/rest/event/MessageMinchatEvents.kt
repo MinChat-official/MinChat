4d198f33b456fd176e8ac6f041aadb97a5fbf671
Mnemotechnician <daytimer253@gmail.com>
2023-03-18 13:22:04 +0300

    Slightly refactored minchat message events

--- minchat-rest/src/main/kotlin/io/minchat/rest/event/MessageMinchatEvents.kt
+++ minchat-rest/src/main/kotlin/io/minchat/rest/event/MessageMinchatEvents.kt
@@ -1,21 +1,33 @@
 package io.minchat.rest.event
 
+import io.minchat.common.entity.Message
 import io.minchat.common.event.*
 import io.minchat.rest.MinchatRestClient
 import io.minchat.rest.entity.withClient
 
+sealed class MinchatMessageEvent<T : Event>(data: T, client: MinchatRestClient) : MinchatEvent<T>(data, client) {
+	protected abstract val dataMessage: Message
+
+	val message = dataMessage.withClient(client)
+	val channel by message::channel
+	val author by message::author
+
+	val authorId by dataMessage.author::id
+	val channelId by dataMessage.channel::id
+}
+
 class MinchatMessageCreate(
 	data: MessageCreateEvent,
 	client: MinchatRestClient
-) : MinchatEvent<MessageCreateEvent>(data, client) {
-	val message = data.message.withClient(client)
+) : MinchatMessageEvent<MessageCreateEvent>(data, client) {
+	override val dataMessage = data.message
 }
 
 class MinchatMessageModify(
 	data: MessageModifyEvent,
 	client: MinchatRestClient
-) : MinchatEvent<MessageModifyEvent>(data, client) {
-	val message = data.message.withClient(client)
+) : MinchatMessageEvent<MessageModifyEvent>(data, client) {
+	override val dataMessage = data.message
 }
 
 class MinchatMessageDelete(
