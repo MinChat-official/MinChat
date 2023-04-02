1ed3319f77b2f31216b87e6c448fe6ec34f96879
Mnemotechnician <daytimer253@gmail.com>
2023-04-02 18:41:17 +0300

    BuildVersion can now be created from a canonical version string

--- minchat-common/src/main/kotlin/io/minchat/common/BuildVersion.kt
+++ minchat-common/src/main/kotlin/io/minchat/common/BuildVersion.kt
@@ -4,8 +4,8 @@ import kotlinx.serialization.Serializable
 
 /**
  * The MinChat version this artifact was built for.
- * Each Minchat artifact contains this property,
  *
+ * Each Minchat artifact contains this property,
  * which it can use to check for compatibility with other
  * artifacts.
  */
@@ -16,13 +16,13 @@ val MINCHAT_VERSION = BuildVersion(
 )
 
 /**
- * Repesents the MinChat version an artifact was built for.
+ * Represents the MinChat version an artifact was built for.
  *
  * This class can be used to check compatibility between two
  * MinChat applications. For example, MinChat clients should
  * request a MinChat build version from the server they connect to
  * before trying to interact with it. If it differs significantly
- * from the build version of the client, it should not try to
+ * from the build version of the client, it shouldn't try to
  * interact with the server to avoid possible conflicts.
  *
  * @param major The major version of this build. If this number differs between
@@ -42,7 +42,7 @@ data class BuildVersion(
 		major == other.major
 	
 	/** Checks if the major and minor versions match. */
-	fun isInterchangableWith(other: BuildVersion) =
+	fun isInterchangeableWith(other: BuildVersion) =
 		isCompatibleWith(other) && minor == other.minor
 	
 	override fun compareTo(other: BuildVersion) = when {
@@ -53,4 +53,37 @@ data class BuildVersion(
 
 	override fun toString() =
 		"$major.$minor.$patch"
+
+	companion object {
+		/**
+		 * Creates a [BuildVersion] from a string in the form of "major.minor.patch".
+		 * Returns null if its invalid.
+		 *
+		 * A version is considered valid if it contains from 2 to 3 components
+		 * separated with dots (e.g. "1.2.3"), all of which are positive integers.
+		 */
+		fun fromStringOrNull(version: String): BuildVersion? {
+			val parts = version.split('.')
+
+			if (parts.size !in 2..3) return null
+
+			val major = parts[0].toIntOrNull() ?: return null
+			val minor = parts[1].toIntOrNull() ?: return null
+			val patch = parts.getOrNull(2)?.let {
+				// Only return null if it's present but invalid
+				it.toIntOrNull() ?: return null
+			} ?: 0
+
+			if (major < 0 || minor < 0 || patch < 0) return null
+
+			return BuildVersion(major, minor, patch)
+		}
+
+		/**
+		 * Creates a [BuildVersion] from a string in the form of "major.minor.patch".
+		 * @throws IllegalArgumentException if the version is invalid.
+		 */
+		fun fromString(version: String) =
+			fromStringOrNull(version) ?: throw IllegalArgumentException("Invalid version string: $version")
+	}
 }
