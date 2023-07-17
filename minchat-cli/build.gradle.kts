2994d7723029d977e6eda04a5d61fc028c047a7e
fox <daytimer253@gmail.com>
2023-07-17 13:57:33 +0300

    ...And remove it again

--- minchat-cli/build.gradle.kts
+++ minchat-cli/build.gradle.kts
@@ -10,8 +10,6 @@ plugins {
 	kotlin("plugin.serialization") version "1.8.0"
 }
 
-apply(plugin = "com.jakewharton.mosaic")
-
 val ktorVersion: String by rootProject
 val picocliVersion: String by rootProject
 
