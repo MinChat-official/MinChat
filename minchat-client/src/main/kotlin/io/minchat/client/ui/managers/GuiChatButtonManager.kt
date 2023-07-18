f9461164d8a080fc4851b6d5132ad64eeb137e62
fox <daytimer253@gmail.com>
2023-07-18 20:38:10 +0300

    Show the gui button even when the minimap is hidden

--- minchat-client/src/main/kotlin/io/minchat/client/ui/managers/GuiChatButtonManager.kt
+++ minchat-client/src/main/kotlin/io/minchat/client/ui/managers/GuiChatButtonManager.kt
@@ -7,9 +7,10 @@ import arc.util.*
 import com.github.mnemotechnician.mkui.extensions.dsl.addTable
 import com.github.mnemotechnician.mkui.extensions.elements.cell
 import io.minchat.client.Minchat
+import io.minchat.client.config.MinchatSettings
 import mindustry.Vars
 import mindustry.game.EventType
-import mindustry.ui.Styles
+import mindustry.ui.*
 
 object GuiChatButtonManager {
 	val button by lazy {
@@ -27,32 +28,37 @@ object GuiChatButtonManager {
 			Core.app.post {
 				lastPlacedContainer?.remove()
 
-//				if (Vars.mobile) {
-//					TODO()
-//				} else if (MinchatSettings.guiButtonDesktop) {
-				val target = Vars.ui.hudGroup.find<Table>("minimap/position")
+				if (Vars.mobile || MinchatSettings.guiButtonDesktop) {
+					val target = Vars.ui.hudGroup.find<Table>("minimap/position")
 
-				if (target == null) {
-					Log.err("Failed to find an appropriate table to place the MinChat chat button.")
-					return@post
+					if (target == null) {
+						Log.err("Failed to find an appropriate table to place the MinChat chat button.")
+						return@post
+					}
+					// Now things get a little tricky: we need the button to be on the top,
+					// so we need to rearrange the table cells.
+					val cell = target.addTable {
+						top().right()
+						add(button)
+					}.fillX()
+					lastPlacedContainer = cell.get()
+
+					// Increase the colspan of the position label and align it to the right
+					target.find<Label>("position").cell()?.apply {
+						val oldColspan = Reflect.get<Int>(this, "colspan")
+						colspan(oldColspan + 1).right()
+					}
+
+					target.cells.remove(cell)
+					target.cells.insert(0, cell) // First row, first colum.
+					target.layout()
+
+					// The user can have the minimap hidden, so we also need to change the visibility of the minimap table
+					target.visible { Vars.ui.hudfrag.shown }
+					target.find<Minimap>("minimap")?.visible {
+						Core.settings.getBool("minimap")
+					}
 				}
-				// Now things get a little tricky: we need the button to be on the top,
-				// so we need to rearrange the table cells.
-				val cell = target.addTable {
-					add(button)
-				}.fillX()
-
-				// Increase the colspan of the position label and align it to the right
-				target.find<Label>("position").cell()?.apply {
-					val oldColspan = Reflect.get<Int>(this, "colspan")
-					colspan(oldColspan + 1).right()
-				}
-
-				lastPlacedContainer = cell.get()
-				target.cells.remove(cell)
-				target.cells.insert(0, cell) // First row, first colum.
-				target.layout()
-//				}
 			}
 		}
 	}
