623f477650a218588b9c7c59244cda98b2feafa6
fox <daytimer253@gmail.com>
2023-11-01 23:35:06 +0300

    New kind of generic dialog

--- minchat-client/src/main/kotlin/io/minchat/client/ui/dialog/Dialogs.kt
+++ minchat-client/src/main/kotlin/io/minchat/client/ui/dialog/Dialogs.kt
@@ -1,8 +1,14 @@
 package io.minchat.client.ui.dialog
 
+import arc.graphics.Color
+import arc.graphics.g2d.*
+import arc.scene.Element
+import arc.util.Align
 import com.github.mnemotechnician.mkui.extensions.dsl.*
 import com.github.mnemotechnician.mkui.extensions.runUi
+import io.minchat.client.Minchat
 import io.minchat.client.misc.Log
+import kotlinx.coroutines.*
 import io.minchat.client.ui.MinchatStyle as Style
 
 /**
@@ -27,6 +33,41 @@ object Dialogs {
 		runUi {
 			ConfirmDialog(message, textNo, textYes, onCancel, onConfirm).show()
 		}
+	}
+
+	/** Asynchronously runs [action] and shows a dialog. Hides the dialog on success, and may show an error dialog on error. */
+	inline fun await(
+		message: String,
+		showError: Boolean = true,
+		cancellable: Boolean = true,
+		crossinline action: suspend () -> Unit
+	) {
+		val job = Minchat.launch {
+			try {
+				action()
+			} catch (e: CancellationException) {
+				// nothing
+			} catch (e: Exception) {
+				Log.error(e) { "Exception in AwaitActionDialog's action" }
+				if (showError) {
+					info("""
+						An error has occurred while processing the action.
+						Check the logs for more detail.
+					""".trimIndent())
+				}
+			}
+		}
+
+		runUi {
+			if (!job.isCancelled && !job.isCompleted) {
+				AwaitActionDialog(message, cancellable, job).show()
+			}
+		}
+	}
+
+	/** Equivalent to calling `info("This action is not implemented yet.")`. */
+	fun TODO() {
+		info("This action is not implemented yet.")
 	}
 
 	class SimpleInfoDialog(val text: String, onClose: () -> Unit = {}) : AbstractModalDialog() {
@@ -86,6 +127,66 @@ object Dialogs {
 					.growX()
 					.pad(Style.layoutPad).row()
 			}.minWidth(300f).pad(Style.layoutPad)
+		}
+	}
+
+	class AwaitActionDialog(
+		val message: String,
+		val cancellable: Boolean = true,
+		val job: Job
+	) : AbstractModalDialog() {
+		override val addCloseAction = false
+
+		init {
+			body.addTable(Style.surfaceBackground) {
+				margin(Style.layoutMargin)
+
+				addLabel(message, wrap = true)
+					.growX()
+					.pad(Style.layoutPad).row()
+
+				// Loading bar animation
+				val start = System.currentTimeMillis()
+				add(object : Element() {
+					override fun draw() {
+						val progress = (System.currentTimeMillis() - start) / 1800f
+						val progWidth = (width - 2 * Style.layoutMargin) * progress
+						val offset = when {
+							progress < 0.5 -> 0f
+							else -> (width - 2 * Style.layoutMargin) * (progress - 0.5f)
+						}
+
+						val midX = getX(Align.center)
+						val midY = getY(Align.center)
+
+						Draw.color(Color.white)
+						Style.surfaceInner.draw(midX, midY, width, height)
+
+						Draw.color(Style.comment)
+						Fill.rect(
+							x + offset + Style.layoutMargin + progWidth / 2,
+							midY,
+							progWidth,
+							height - 2 * Style.layoutMargin
+						)
+					}
+				}).growX().minHeight(50f).pad(Style.layoutPad)
+
+				if (cancellable) {
+					action("Cancel") {
+						job.cancel()
+						hide()
+					}
+				}
+			}.minWidth(300f).pad(Style.layoutPad)
+		}
+
+		override fun act(delta: Float) {
+			if (!job.isActive) {
+				hide()
+			}
+
+			super.act(delta)
 		}
 	}
 }
